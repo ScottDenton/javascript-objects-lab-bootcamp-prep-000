2a618b2cb39d51146136ad411c7d58d7b8aa4905
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-11 16:05:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function updateObjectWithKeyAndValue (object, key, value){
 }
 
 function destructivelyUpdateObjectWIthKeyAndValue (object, key, value) {
-  return object.key = value;
+  object.key = value;
+  return object;
 }
